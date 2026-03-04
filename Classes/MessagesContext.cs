495394d9982738554bdab57f28bfdf99db48f44a
STEPS0N <stepan.klimoff@mail.ru>
2026-03-04 18:49:01 +0500

    Разработанные контексты данных. Климов

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,26 @@
+﻿using ChatStudents_Klimov.Classes.Common;
+using ChatStudents_Klimov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Klimov.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+
+        public MessagesContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.config);
+        }
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,27 @@
+﻿using ChatStudents_Klimov.Classes.Common;
+using ChatStudents_Klimov.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Klimov.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.config);
+        }
+    }
+}
