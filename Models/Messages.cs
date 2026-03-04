3b655a707be5a753f866229e00b281120b3f3bd0
STEPS0N <stepan.klimoff@mail.ru>
2026-03-04 18:05:59 +0500

    Разработанные модели. Климов

---
+++ Models/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Klimov.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserForm { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+
+        public Messages(int UserForm, int UserTo, string Message)
+        {
+            this.UserForm = UserForm;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Klimov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surename { get; set; }
+        public byte[] Photo { get; set; }
+
+        public Users(string Lastname, string Firstname, string Surename, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surename = Surename;
+            this.Photo = Photo;
+        }
+
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surename}";
+        }
+    }
+}
