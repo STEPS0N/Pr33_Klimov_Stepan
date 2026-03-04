3ee1d3ec59ee762ad62076fc01ab0c986ea188db
STEPS0N <stepan.klimoff@mail.ru>
2026-03-04 18:12:23 +0500

    Разработанные общие классы. Климов

---
+++ Classes/Common/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Klimov.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=student.permaviat.ru;" +
+            "Trusted_Connection=False;" +
+            "Database=***;" +
+            "User=***" +
+            "PWD=;";
+    }
+}
