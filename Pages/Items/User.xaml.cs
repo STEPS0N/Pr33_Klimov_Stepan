6561c80ae27129c5cf64dd4f342f8e12488145fa
STEPS0N <stepan.klimoff@mail.ru>
2026-03-04 20:19:27 +0500

    Разработанный пользовательский элемент User. Климов

---
+++ Pages/Items/User.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ChatStudents_Klimov.Classes.Common;
+using ChatStudents_Klimov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudents_Klimov.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        Users user;
+        Main main;
+
+        public User(Users user, Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFIO();
+        }
+
+        private void SelectChat(object sender, MouseButtonEventArgs e)
+        {
+            main.SelectUser(user);
+        }
+    }
+}
